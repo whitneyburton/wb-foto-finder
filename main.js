ac2de8ccaf60495a587fa319126bc98c5a58e299
Whitney Burton <whitneyburton9@gmail.com>
2018-11-03 17:38:07 -0600

    Add save to storage functionality

--- main.js
+++ main.js
@@ -2,28 +2,38 @@
 // If there is no else, how could you leverage an else
 // Grab the whole object from storage. Not individuals properties at once
 
-var addToAlbumButton = document.querySelector('.add-to-album-button');
+// var addToAlbumButton = document.querySelector('.add-to-album-button');
 var titleInput =  document.querySelector('.title-input');
 var captionInput = document.querySelector('.caption-input');
 
-addToAlbumButton.addEventListener('click', fotoCardProperties);
+document.querySelector('.add-to-album-button').addEventListener('click', fotoCardProperties);
+document.getElementById('card-article').addEventListener('click', removeFotoCard);
+
+reloadCards();
+
+function reloadCards() {
+  document.querySelector('.foto-form').reset();
+    Object.keys(localStorage).forEach(function(key) {
+      populateFotoCard(JSON.parse(localStorage.getItem(key)));
+  })
+};
 
 function fotoCardProperties(e) {
   e.preventDefault();
   var newFotoObj = new Foto(titleInput.value, captionInput.value);
-  // newFotoObj.setToStorage();
+  newFotoObj.saveToStorage();
   populateFotoCard(newFotoObj);
-  // document.querySelector('.foto-form').reset();
+  document.querySelector('.foto-form').reset();
 }
 
 function populateFotoCard(newFotoObj) {
   var card = document.createElement('section');
   var cardArticle = document.getElementById('card-article');
-  card.className="card";
+  card.className='card';
   card.id = newFotoObj.id;
   // dataset.index here for id
   card.innerHTML = 
-    `<div class="card">
+    `<div class="card-wrapper">
       <h4 class="card-title" contenteditable="true">${newFotoObj.title}</h4>
       <div class="uploaded-image">
         <img class="uploaded-image" src="images/waterfall.jpg">
@@ -36,3 +46,13 @@ function populateFotoCard(newFotoObj) {
     </div>`;
   cardArticle.append(card);
 };
+
+function removeFotoCard(e) {
+  if (e.target.className === 'delete-icon') {
+    var id = e.target.closest('.card'.id);
+    var deleteMethodObj = new Foto('', '', id);
+    // console.log(deleteMethodObj);
+    deleteMethodObj.deleteFromStorage();
+    e.target.closest('.card').remove();
+  }
+}
